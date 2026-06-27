79eeeeed309d997cc2f4945aeb209ee7ae78834f
UlumDcode <apumm12@gmail.com>
2026-06-27 21:17:31 +0700

    Etch-a-Sketch

--- script.js
+++ script.js
@@ -12,9 +12,26 @@ function createGrid(size) {
 
     row.style.width = `${100 / size}%`;
     row.style.height = `${100 / size}%`;
+    row.style.background = "white";
+
+    row.addEventListener("mouseenter", () => {
+      row.style.background = "#111111";
+    });
 
     container.appendChild(row);
   }
 }
 
 createGrid(n);
+
+resize.addEventListener("click", () => {
+  let i = Number(prompt(`Enter grid (min 1 max 100) :`));
+  if (i >= 1 && i <= 100) {
+    n = i;
+    createGrid(n);
+  }
+});
+
+reset.addEventListener("click", () => {
+  createGrid(n);
+});
