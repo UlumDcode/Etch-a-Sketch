bcab5534d4b46c3923a81c5a65e7af323c504d97
UlumDcode <apumm12@gmail.com>
2026-06-27 20:59:25 +0700

    Etch-a-Sketch

--- script.js
+++ script.js
@@ -6,9 +6,14 @@ let n = 16; //default size
 
 function createGrid(size) {
   container.innerHTML = "";
-  for (let i = 0; i < size; i++) {
+  for (let i = 0; i < size ** 2; i++) {
     const row = document.createElement("div");
     row.classList.add("row");
+
+    row.style.width = `${100 / size}%`;
+    row.style.height = `${100 / size}%`;
+
+    container.appendChild(row);
   }
 }
 
